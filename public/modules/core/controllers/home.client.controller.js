0c1aefcae7fdd6eb1317e36306796bb28875a69b
DmaC <demon.005.gl@gmail.com>
2019-03-16 19:10:42 -0500

    project structure fix view change

---
+++ public/modules/core/controllers/home.client.controller.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+angular.module('core').controller('HomeController', ['$scope', '$http',
+	function($scope, $http) {
+		// This provides Authentication context.
+		$scope.formData = {title: 'test', description: ''};
+
+		// when landing on the page, get all todos and show them
+		$scope.getPosts = function() {
+			$http.get('/api/questions')
+				.success(function(data) {
+					$scope.todos = data;
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+		};
+
+
+		// when submitting the add form, send the text to the node API
+		$scope.createTodo = function() {
+			$http.post('/api/questions', $scope.formData)
+				.success(function(data) {
+					$scope.formData = {title: 'test', description: ''}; // clear the form so our user is ready to enter another
+					$scope.getPosts();
+					console.log(data);
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+		};
+
+		// delete a todo after checking it
+		$scope.deleteTodo = function(id) {
+			$http.delete('/api/todos/' + id)
+				.success(function(data) {
+					$scope.todos = data;
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+		};
+	}
+]);
