c88717bdd59a55a31130bc7bd554b13113ecc4e0
anherdene <anherdene@gmail.com>
2019-03-19 11:50:32 -0500

    added reaction test

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ let expect = chai.expect;
 it('create post', function() {
     expect(posts._createPost("test",1)).to.include.deep.members([{
         "id": 1,
-        "content": "In computer science, functional programming is a programming paradigm—a style of building the structure and elements of computer programs—that treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data.",
+        "content": "Reserve army of labour is a concept in Karl Marx's critique of political economy.[1] It refers to the unemployed and underemployed in capitalist society. It is synonymous with \"industrial reserve army\" or \"relative surplus population\", except that the unemployed can be defined as those actually looking for work and that the relative surplus population also includes people unable to work. ",
         "created_date": "2019-03-14T18:25:43.511Z",
         "user_id": 1
     }]);
@@ -34,7 +34,7 @@ it('create like', function() {
 it('get posts', function() {
     expect(posts._listPost(1)).to.include.deep.members([{
         "id": 1,
-        "content": "In computer science, functional programming is a programming paradigm—a style of building the structure and elements of computer programs—that treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data.",
+        "content": "Reserve army of labour is a concept in Karl Marx's critique of political economy.[1] It refers to the unemployed and underemployed in capitalist society. It is synonymous with \"industrial reserve army\" or \"relative surplus population\", except that the unemployed can be defined as those actually looking for work and that the relative surplus population also includes people unable to work. ",
         "created_date": "2019-03-14T18:25:43.511Z",
         "user_id": 1
     }]);
@@ -48,6 +48,7 @@ it('create user', function() {
         "lastname": "Dulamsuren",
         "password": "dGVzdA==",
         "gender": "male",
+        "img": "Demo.jpg",
         "created_date": "2019-03-14T18:25:43.511Z"
     }]);
 });
@@ -74,13 +75,14 @@ it('list user  non followers', function() {
 
 it('list user  _listUsersById', function() {
     expect(users._listUsersById([1])).to.have.deep.members([ {
-        "created_date": "2019-03-14T18:25:43.511Z",
-        "firstname": "Byambadorj",
-        "gender": "male",
         "id": 1,
+        "username": "Byambaa",
+        "firstname": "Byambadorj",
         "lastname": "Dulamsuren",
         "password": "dGVzdA==",
-        "username": "Byambaa"
+        "gender": "male",
+        "img": "Demo.jpg",
+        "created_date": "2019-03-14T18:25:43.511Z"
     }]);
 });
 it('search user by searchtext', function() {
@@ -91,15 +93,16 @@ it('search user by searchtext', function() {
         "lastname": "Bold",
         "password": "dGVzdA==",
         "gender": "male",
+        "img": "Anhaa.jpg",
         "created_date": "2019-03-14T18:25:43.511Z"
     }]);
 });
 it('search post by searchtext', function() {
-    expect(posts._searchPost("In computer science, functional programming")).to.include.deep.members([ {
-        "id": 1,
+    expect(posts._searchPost("In computer science, functional programming")).to.include.deep.members([{
+        "id": 11,
         "content": "In computer science, functional programming is a programming paradigm—a style of building the structure and elements of computer programs—that treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data.",
         "created_date": "2019-03-14T18:25:43.511Z",
-        "user_id": 1
+        "user_id": 2
     }]);
 });
 
